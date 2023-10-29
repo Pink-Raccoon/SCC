2699f2b5ea938b12452b92e925ceee7ea7f21caa
Pink-Raccoon <schweash@students.zhaw.ch>
2023-10-29 16:13:34 +0100

    lab4

--- Lab4/MaterialPython/task1.py
+++ Lab4/MaterialPython/task1.py
@@ -3,14 +3,20 @@ import load_data as ld
 
 
 matrices = ld.loadData()
-trainLabels = matrices['trainLabels']
+trainLabels1 = matrices['trainLabelsEx1']
+trainLabels2 = matrices['trainLabelsEx2']
 trainMatrixEx1 = matrices['trainMatrixEx1']
 trainMatrixEx2 = matrices['trainMatrixEx2']
 
-def train(matrix):
+testLabels1 = matrices['testLabelsEx1']
+testLabels2 = matrices['testLabelsEx2']
+testMatrixEx1 = matrices['testLabelsEx1']
+testMatrixEx2 = matrices['testLabelsEx2']
+
+def train(matrix,trainlabels):
     # 1 = spam, 0 = non spam
-    spams = np.where(trainLabels == 1)[0] # Indices of which rows in matrix are spam
-    nonSpams = np.where(trainLabels == 0)[0] # Indices of which rows in matrix are non spam
+    spams = np.where(trainlabels == 1)[0] # Indices of which rows in matrix are spam
+    nonSpams = np.where(trainlabels == 0)[0] # Indices of which rows in matrix are non spam
 
     spamDocs = np.array([]).astype(int)
     nonSpamDocs = np.array([]).astype(int)
@@ -30,13 +36,13 @@ def train(matrix):
     totalSpamWords = np.sum(spamOccurences)
     totalNonSpamWords = np.sum(nonSpamOccurences)
 
-    print("total Spam documents: {}".format(totalSpam))
-    print("total non spam docs: {}".format(totalNonSpam))
-    print("train matrix {}".format(matrix))
-    print("spam Occurences {}".format(spamOccurences))
-    print("non spam occurences {}".format(nonSpamOccurences))
-    print("total spam word count {}".format(totalSpamWords))
-    print("total non spam word count {}".format(totalNonSpamWords))
+    # print("total Spam documents: {}".format(totalSpam))
+    # print("total non spam docs: {}".format(totalNonSpam))
+    # print("train matrix {}".format(matrix))
+    # print("spam Occurences {}".format(spamOccurences))
+    # print("non spam occurences {}".format(nonSpamOccurences))
+    # print("total spam word count {}".format(totalSpamWords))
+    # print("total non spam word count {}".format(totalNonSpamWords))
 
     probTokensSpam = spamOccurences / totalSpamWords
     if len(probTokensSpam) < 11:
@@ -46,6 +52,6 @@ def train(matrix):
          print('probTokensNonSpam ', probTokensNonSpam)
     return probTokensSpam, probTokensNonSpam, probSpam, numTokens
 
-train(trainMatrixEx1)
-train(trainMatrixEx2)
+# train(trainMatrixEx1,trainLabels1)
+# train(trainMatrixEx2,trainLabels2)
 
--- Lab4/MaterialPython/task1b.py
+++ Lab4/MaterialPython/task1b.py
@@ -1,9 +1,49 @@
 import numpy as np
 import load_data as ld
+import task1 as tk
+
 
 matrices = ld.loadData()
-trainLabels = matrices['testLabels']
-trainMatrixEx1 = matrices['testMatrixEx1']
+trainLabels1 = matrices['trainLabelsEx1']
+trainLabels2 = matrices['trainLabelsEx2']
+trainMatrixEx1 = matrices['trainMatrixEx1']
+trainMatrixEx2 = matrices['trainMatrixEx2']
 
-def test(matrix):
-    
+testLabels1 = matrices['testLabelsEx1']
+testLabels2 = matrices['testLabelsEx2']
+testMatrixEx1 = matrices['testLabelsEx1']
+testMatrixEx2 = matrices['testLabelsEx2']
+
+# probTokensSpam, probTokensNonSpam, probSpam, numTokens = tk.train(trainMatrixEx1,trainLabels1)
+probTokensSpam, probTokensNonSpam, probSpam, numTokens = tk.train(trainMatrixEx2,trainLabels2)
+def test(matrix, testlabels):
+    numberOfDocs = len(matrix)
+    pMailSpam = np.zeros(numberOfDocs)
+    pMailNonSpam = np.zeros(numberOfDocs)
+    output = []
+
+    for i in range(numberOfDocs):
+        occurencesDoc = matrix[i]
+        prod1 = 1
+        prod2 = 1
+
+        for j in range(numTokens):
+            prod1 *= probTokensSpam[j] ** occurencesDoc
+            prod2 *= probTokensNonSpam[j] ** occurencesDoc
+
+        pMailSpam[i] = prod1
+        pMailNonSpam[i] = prod2
+
+        PmailAndSpam = pMailSpam * probSpam
+        PmailAndNonSpam = pMailNonSpam * (1 - probSpam)
+        
+    classification = (PmailAndSpam > PmailAndNonSpam).astype(int)  # 1 for spam, 0 for non-spam
+    output.append(classification)
+
+    error_rate = sum(testlabels != output)/numberOfDocs
+    print("Output: {}".format(output))
+    print("Error rate: {}".format(error_rate))
+
+
+#test(testMatrixEx1, testLabels1)
+test(testMatrixEx2,testLabels2)
